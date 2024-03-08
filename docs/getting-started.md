32aa1fa6483e29b7d03e483c60d05466e192f244
DanielMA <danoan2008@gmail.com>
2024-03-08 15:43:02 +0100

    Initial commit

---
+++ docs/getting-started.md
@@ -0,0 +1,13 @@
+# Getting started with ChatGPT Dictionary
+
+Multilanguage dictionary using ChatGPT
+
+## Features
+
+
+## Examples
+
+
+## Contributing
+
+Please reference to our [contribution](http://danoan.github.io/chatgpt-dictionary/contributing) and [code-of-conduct]((http://danoan.github.io/chatgpt-dictionary/code-of-conduct.md)) guidelines.
---
+++ test/integration/core/api/test_api.py
@@ -0,0 +1,67 @@
+from danoan.dictionaries.chatgpt.core import api
+
+import json
+import pytest
+import warnings
+
+
+@pytest.fixture(scope="session")
+def openai_key(pytestconfig):
+    v = pytestconfig.getoption("openai_key")
+    if v is None:
+        warnings.warn("The openai_key is not specified. Tests won't be executed.")
+    return pytestconfig.getoption("openai_key", skip=True)
+
+
+@pytest.mark.api
+@pytest.mark.parametrize(
+    "word,language",
+    [
+        ("hapiness", "eng"),
+        ("state", "eng"),
+        ("clafoutis", "fra"),
+        ("pareil", "fra"),
+        ("magari", "ita"),
+        ("profumo", "ita"),
+    ],
+)
+def test_get_definition(openai_key, word, language):
+    response = api.get_definition(openai_key, word, language)
+    assert response
+    assert len(response) > 10
+
+
+@pytest.mark.api
+@pytest.mark.parametrize(
+    "word,language",
+    [
+        ("terrible", "eng"),
+        ("passion", "eng"),
+        ("ensemble", "fra"),
+        ("travail", "fra"),
+        ("cibo", "ita"),
+        ("facile", "ita"),
+    ],
+)
+def test_get_synonyme(openai_key, word, language):
+    response = api.get_definition(openai_key, word, language)
+    assert response
+    assert len(response) > 10
+
+
+@pytest.mark.api
+@pytest.mark.parametrize(
+    "text,language,mandate_words",
+    [
+        ("that thing we put our plates on whenever we have a meal", "eng", ["table"]),
+        ("ce vêtement qu'on utilise quand on a froid", "fra", ["manteau"]),
+        ("una bevanda nera, aromatica ed energetica", "ita", ["caffè"]),
+    ],
+)
+def test_get_reverse_definition(openai_key, text, language, mandate_words):
+    response = api.get_reverse_definition(openai_key, text, language)
+    assert response
+    assert len(response) > 10
+    obj = json.loads(response)
+    for word in mandate_words:
+        assert word in obj
