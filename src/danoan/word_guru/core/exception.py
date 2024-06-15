3565806d2e5b406f8be5ce8e87ad8a775a52c5b7
DanielMA <danoan2008@gmail.com>
2024-06-15 17:36:40 +0200

    Throw exception if language not recognized

--- src/danoan/word_guru/core/exception.py
+++ src/danoan/word_guru/core/exception.py
@@ -1,2 +1,10 @@
 class OpenAIEmptyResponse(Exception):
     pass
+
+
+class LanguageCodeNotRecognizedError(Exception):
+    def __init__(self, language_code: str):
+        self.language_code = language_code
+
+    def __str__(self):
+        return f"The language code {self.language_code} is not recognized. Make sure to enter a valid ISO 639-3 code. For example, `eng` for English"
