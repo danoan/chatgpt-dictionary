4c22f310909f34f3eedb9ea99aeafcd22f507e54
DanielMA <danoan2008@gmail.com>
2024-06-23 20:09:10 +0200

    fix cache folder

--- src/danoan/word_guru/cli/commands/dictionary_commands/get_reverse_definition.py
+++ src/danoan/word_guru/cli/commands/dictionary_commands/get_reverse_definition.py
@@ -31,6 +31,8 @@ def get_reverse_definition(
                 "The given cache folder does not exist. Create one before proceeding."
             )
             exit(1)
+    else:
+        cache_folder_path = None
 
     try:
         print(api.get_reverse_definition(openai_key, cache_folder_path, text, language))
